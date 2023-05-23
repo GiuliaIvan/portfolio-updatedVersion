376aada3a8e5365f99036c49f2acec2e2c57053a
Giulia Ivan <eaagril@students.eaaa.dk>
2023-05-23 17:29:12 +0200

    Update experience.jsx

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import './experience.css'
-import {BsPatchCheckFill} from 'react-icons/bs'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <section id='experience'>
+    <section id="experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
 
@@ -12,44 +12,42 @@ const Experience = () => {
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-            <article className='experience__details'>
-              <BsPatchCheckFill/>
+            <article className="experience__details">
+              <BsPatchCheckFill />
               <h4>HTML</h4>
-              <small className='text-light'>Experienced</small>
+              <small className="text-light">Experienced</small>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill/>
+            <article className="experience__details">
+              <BsPatchCheckFill />
               <h4>CSS</h4>
-              <small className='text-light'>Intermediate</small>
+              <small className="text-light">Intermediate</small>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill/>
+            <article className="experience__details">
+              <BsPatchCheckFill />
               <h4>JavaScript</h4>
-              <small className='text-light'>Experienced</small>
+              <small className="text-light">Experienced</small>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill/>
+            <article className="experience__details">
+              <BsPatchCheckFill />
               <h4>Bootstrap</h4>
-              <small className='text-light'>Experienced</small>
+              <small className="text-light">Experienced</small>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill/>
+            <article className="experience__details">
+              <BsPatchCheckFill />
               <h4>Tailwind</h4>
-              <small className='text-light'>Experienced</small>
+              <small className="text-light">Experienced</small>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill/>
+            <article className="experience__details">
+              <BsPatchCheckFill />
               <h4>React</h4>
-              <small className='text-light'>Experienced</small>
+              <small className="text-light">Experienced</small>
             </article>
           </div>
         </div>
-        <div className="experience__backend">
-          
-        </div>
+        <div className="experience__backend"></div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
