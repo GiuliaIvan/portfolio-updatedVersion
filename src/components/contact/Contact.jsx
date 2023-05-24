0f6fea334475ce63a8c461b9635d5f8d036db590
Giulia Ivan <eaagril@students.eaaa.dk>
2023-05-24 11:39:06 +0200

    contact section styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,29 +13,34 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>dummyegator@gmail.com</h5>
-            <a href="mailto:dummyegator@gmail.com">Send a message</a>
+            <a href="mailto:dummyegator@gmail.com" target='_blank' rel='noreferrer'>Send a message</a>
           </article>
 
           <article className="contact__option">
-            <RiMessengerLine />
+            <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>egatortutorials</h5>
-            <a href="https://m.me/giulia.ivan">Send a message</a>
+            <a href="https://m.me/giulia.ivan" target='_blank' rel='noreferrer'>Send a message</a>
           </article>
 
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+4555220055</h5>
-            <a href="https://api.whatsapp.com/send?phone=+4555220055">Send a message</a>
+            <a href="https://api.whatsapp.com/send?phone=+4555220055" target='_blank' rel='noreferrer'>Send a message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
 
-        <form action=""></form>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
