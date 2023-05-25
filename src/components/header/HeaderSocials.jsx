0477459742ddbf14b2d02cf4b96cea817202cef7
Giulia Ivan <eaagril@students.eaaa.dk>
2023-05-25 10:46:01 +0200

    socials links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
-import {FiDribbble} from 'react-icons/fi'
+import { FiInstagram } from "react-icons/fi";
 
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
-        <a href="https://linkedin.com" rel='noreferrer' target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com" rel='noreferrer' target='_blank'><FaGithub/></a>
-        <a href="https://dribbble.com" rel='noreferrer' target='_blank'><FiDribbble/></a>
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/giulia-ivan-let-601a9121b"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/GiuliaIvan" rel="noreferrer" target="_blank">
+        <FaGithub />
+      </a>
+      <a
+        href="https://www.instagram.com/giuli_ivan/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <FiInstagram />
+      </a>
     </div>
-  )
+  );
 }
 
 export default HeaderSocials
