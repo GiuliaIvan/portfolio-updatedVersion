f10b06547ca619455dd2d1fcbffc7a7416a9a3b5
Giulia Ivan <eaagril@students.eaaa.dk>
2023-05-26 11:15:59 +0200

    testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './testimonials.css'
-import AVTR1 from '../../assets/avatar1.jpg'
-import AVTR2 from '../../assets/avatar2.jpg'
-import AVTR3 from '../../assets/avatar3.jpg'
-import AVTR4 from '../../assets/avatar4.jpg'
+import AVTR1 from '../../assets/avatar1.png'
+import AVTR2 from '../../assets/avatar2.png'
+import AVTR3 from '../../assets/portfolio7.jpg'
+import AVTR4 from '../../assets/portfolio8.jpg'
 
 // import Swiper core and required modules
 import { Pagination } from 'swiper';
@@ -18,28 +18,32 @@ import 'swiper/css/pagination';
 const data = [
   {
     avatar: AVTR1,
-    name: 'Tina Snow',
-    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    name: "PICit",
+    review:
+      "Collaborating with Giulia on our web page redesign project was an exceptional experience. Her creativity, attention to detail, and understanding of our brand resulted in a visually stunning and user-friendly website. Giulia communicated effectively, incorporated feedback seamlessly, and delivered exceptional results. We highly recommend her services to anyone in need of a talented and reliable web designer.",
   },
 
   {
     avatar: AVTR2,
-    name: 'Tina Snow',
-    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    name: "Andreea - CoroBeauty",
+    review:
+      "Collaborating with Giulia was a game-changer for my business. Her expertise in multimedia design elevated my small-company to new heights. She not only brought our place vision to life but she was also a great communicator, always keeping us updated on the progress.. Her dedication and professionalism made her a valuable asset to me. I highly endorse Giulia for any kind of digital projects.",
   },
 
   {
     avatar: AVTR3,
-    name: 'Tina Snow',
-    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    name: "Café Her Og Der",
+    review:
+      "We had the privilege of collaborating with Giulia on a project, and we couldn't be happier with the results. Her creativity and technical skills transformed our design concepts into an engaging and user-friendly website. She was receptive to feedback and always went the extra mile to exceed our expectations. Giulia is a true professional, and we look forward to working with her again in the future.",
   },
 
   {
     avatar: AVTR4,
-    name: 'Tina Snow',
-    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    name: "Kromosom",
+    review:
+      "Working with Giulia was an absolute pleasure. Her expertise in frontend development brought our project to life. She paid attention to every detail, ensuring that our website not only looked visually stunning but also functioned flawlessly. Her professionalism, clear communication, and ability to meet deadlines made the collaboration seamless. We are thrilled with the final product and would highly recommend Giulia for any frontend development needs.",
   },
-]
+];
 
 const Testimonials = () => {
   return (
