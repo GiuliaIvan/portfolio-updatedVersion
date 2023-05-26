e234fed573c9bae7f52ae4475ae0dfaf67c89f55
Giulia Ivan <eaagril@students.eaaa.dk>
2023-05-26 16:45:42 +0200

    Update experience.jsx

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -43,7 +43,7 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
-                <h4>Tailwind</h4>
+                <h4>SCSS</h4>
                 <small className="text-light">Basic</small>
               </div>
             </article>
