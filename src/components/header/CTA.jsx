10f5c9de38b70a03872f2d9bd601dcb35f5fa462
Giulia Ivan <eaagril@students.eaaa.dk>
2025-03-08 11:38:34 +0100

    new CV uploaded

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 // import CV from '../../assets/GiuliaIvanResume.pdf'
 // import CV from "../../assets/GiuliaIvanResume1.pdf";
-import CV from "../../assets/GiuliaIvanResume2.pdf";
+// import CV from "../../assets/GiuliaIvanResume2.pdf";
+import CV from "../../assets/GiuliaIvan-Resume.pdf";
 
 const CTA = () => {
   return (
