4217ea9751a5494e07b376f3700af7928848fec1
Giulia Ivan <eaagril@students.eaaa.dk>
2025-03-08 11:36:28 +0100

    about sections modified

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -38,9 +38,17 @@ const About = () => {
           </div>
 
           <p>
-            I'm Frontend developer who helps people materialize their dream into a
+            {/* I'm a Frontend developer and Designer who helps people materialize their dream into a
             beautiful, hands-on prototype. Creative, insightful and passionate
-            about writing, videography and volley.
+            about writing, videography and volley. */}
+            Beyond the technical side,{" "}
+            <strong style={{ color: "white" }}>teamwork</strong> and{" "}
+            <strong style={{ color: "white" }}>creative problem-solving</strong>{" "}
+            are my strengths. Whether it's brainstorming concepts, refining
+            designs, or finding the perfect balance between aesthetics and
+            functionality, I believe great ideas are shaped through{" "}
+            <strong style={{ color: "white" }}>collaboration.</strong> Maybe
+            that’s why I love volleyball so much hm...<strong style={{ color: "white" }}>🤔🏐</strong>
           </p>
 
           <a href="#contact" className="btn btn-primary">
